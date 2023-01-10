0079fcc871a486ba3955313ef119790ebcd5f5d9
Kate <3837437+katesroad@users.noreply.github.com>
2023-01-09 23:56:08 -0500

    Add mapStatusToDotVariant

---
+++ src/utils/feedback.ts
@@ -0,0 +1,11 @@
+import { DotProps } from '@/components/Dot';
+
+const DOT_VARIANT_MAP: Record<Entities.Feedback.TRoadmapStatus, DotProps['variant']> = {
+  planned: 'orange',
+  'in progress': 'purple',
+  live: 'blue',
+};
+
+export const mapStatusToDotVariant = (status: Entities.Feedback.TRoadmapStatus) => {
+  return DOT_VARIANT_MAP[status];
+};
