c91ae0319f10fd28dfb590d65f85eb413a86b0a4
Kate <3837437+katesroad@users.noreply.github.com>
2023-01-09 21:46:00 -0500

    Add auth check for form submition in AddComemnt

--- src/pages/feedbacks/DetailPage/components/AddComment/AddComment.tsx
+++ src/pages/feedbacks/DetailPage/components/AddComment/AddComment.tsx
@@ -4,6 +4,7 @@ import { useForm, FormProvider, FieldValues } from 'react-hook-form';
 
 import Button from '@/components/Button';
 import { Textarea } from '@/components/Form';
+import RequireAuth from '@/components/RequireAuth';
 
 import { useAddFeedbackComment } from '@/hooks/queries/feedbacks';
 
@@ -14,13 +15,6 @@ const MAX_LENGTH = 250;
 export const AddComment: React.FC<Props> = ({ toFeedback }) => {
   const mutation = useAddFeedbackComment();
 
-  const onSubmit = ({ detail }: FieldValues) => {
-    mutation.mutate({
-      detail,
-      feedbackId: toFeedback,
-    } as never);
-  };
-
   const methods = useForm({
     mode: 'onChange',
     defaultValues: {
@@ -28,44 +22,57 @@ export const AddComment: React.FC<Props> = ({ toFeedback }) => {
     },
   });
 
+  const onSubmit = async ({ detail }: FieldValues) => {
+    const result = await mutation.mutateAsync({
+      detail,
+      feedbackId: toFeedback,
+    } as never);
+
+    if ((result as Entities.TComment).id) {
+      methods.reset();
+    }
+  };
+
   const remainingLength = MAX_LENGTH - methods.watch('detail').length;
 
   return (
     <FormProvider {...methods}>
-      <form
-        className="add-comment__form"
-        onSubmit={methods.handleSubmit(onSubmit)}
-      >
-        <Textarea
-          aria-label="comment content"
-          fullWidth
-          name="detail"
-          placeholder="Type your comment here"
-          required={{
-            value: true,
-            message: '',
-          }}
-        />
-        <div className="add-comment__action">
-          <span
-            aria-label="remaining comment character"
-            className={classNames('add-comment__reminder', {
-              'add-comment__reminder--error': (remainingLength < 0)
-            })}
-          >
-            {(remainingLength > 0)
-              ? `${remainingLength} characters left`
-              : 'Comment is too long'}
-          </span>
-          <Button
-            disabled={remainingLength < 0}
-            type="submit"
-          >
-            Post Comment
-          </Button>
-        </div>
-      </form>
-    </FormProvider >
+      <RequireAuth actionName="onSubmit">
+        <form
+          className="add-comment__form"
+          onSubmit={methods.handleSubmit(onSubmit)}
+        >
+          <Textarea
+            aria-label="comment content"
+            fullWidth
+            name="detail"
+            placeholder="Type your comment here"
+            required={{
+              value: true,
+              message: '',
+            }}
+          />
+          <div className="add-comment__action">
+            <span
+              aria-label="remaining comment character"
+              className={classNames('add-comment__reminder', {
+                'add-comment__reminder--error': remainingLength < 0,
+              })}
+            >
+              {remainingLength > 0
+                ? `${remainingLength} characters left`
+                : 'Comment is too long'}
+            </span>
+            <Button
+              disabled={remainingLength < 0}
+              type="submit"
+            >
+              Post Comment
+            </Button>
+          </div>
+        </form>
+      </RequireAuth>
+    </FormProvider>
   );
 };
 
