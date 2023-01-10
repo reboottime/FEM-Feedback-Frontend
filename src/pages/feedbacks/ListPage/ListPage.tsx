bb8cbd89253b590159fad321626fe8cfaa422d84
Kate <3837437+katesroad@users.noreply.github.com>
2023-01-10 13:23:56 -0500

    Load feedbacks from backend and add basic layout for mobile version

--- src/pages/feedbacks/ListPage/ListPage.tsx
+++ src/pages/feedbacks/ListPage/ListPage.tsx
@@ -1,15 +1,26 @@
+import classsNames from 'classnames';
 import React, { useState } from 'react';
 
 import Categories from './components/Categories';
+import Feedbacks from './components/Feedbacks';
 import Roadmap from './components/Roadmap';
 import Sidebar from './components/Sidebar';
+import SortAndAdd from './components/SortAndAdd';
 
 import { useIsMobile } from '@/hooks/mediaQueries';
+import { useGetFeedbacks } from '@/hooks/queries/feedbacks';
 
 import './style.scss';
 
 export const ListPage = () => {
   const isMobile = useIsMobile();
+
+  const {
+    data: feedbacks,
+    isError: isFeedbacksError,
+    isLoading: isFeedbacksLoading,
+    isSuccess: isFeedbacksLoaded,
+  } = useGetFeedbacks();
 
   const [category, setCategory] = useState<TCategory>('All');
 
@@ -29,6 +40,14 @@ export const ListPage = () => {
           />
         </Sidebar>
       )}
+      <main className={classsNames('list-page__main', {
+        'list-page__main--mobile': isMobile
+      })}>
+        <SortAndAdd />
+        {isFeedbacksLoading && <p>Loading...</p>}
+        {isFeedbacksError && <p>Failed to load</p>}
+        {isFeedbacksLoaded && <Feedbacks feedbacks={feedbacks as unknown as Entities.Feedback.TFeedback[]} />}
+      </main>
     </div>
   );
 };
---
+++ src/pages/feedbacks/ListPage/components/Feedbacks/Feedbacks.tsx
@@ -0,0 +1,42 @@
+import classNames from 'classnames';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import NoData from './NoData';
+
+import Feedback from '@/components/Feedback';
+
+import './style.scss';
+
+export const Feedbacks: React.FC<Props> = ({ feedbacks }) => (
+  <div className={classNames('feedbacks', {
+    'feedbacks--no-data border-rounded--large': (feedbacks.length === 0),
+  })}
+  >
+    {feedbacks.length
+      ? (
+        <ul className="feedbacks__list">
+          {feedbacks.map((feedback) => (
+            <li
+              className="feedbacks__list-item"
+              key={feedback.id}
+            >
+              <Link
+                className="feedbacks__item-link border-rounded--large"
+                to={`/feedbacks/${feedback.id}`}
+              >
+                <Feedback {...feedback} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )
+      : (
+        <NoData />
+      )}
+  </div>
+);
+
+interface Props {
+  feedbacks: Entities.Feedback.TFeedback[];
+}
