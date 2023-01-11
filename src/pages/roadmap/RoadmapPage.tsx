50ce08648ca6831ea50aa1c9907839dc860dc708
Kate <3837437+katesroad@users.noreply.github.com>
2023-01-10 20:25:23 -0500

    Add roadmap page mobile header

--- src/pages/roadmap/RoadmapPage.tsx
+++ src/pages/roadmap/RoadmapPage.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
 
+import Goback from '@/components/Goback';
+
+import { AuthContextType, useAuthContext } from '@/components/AppProviders';
+import AddButton from '@/components/AddButton';
+import UserIcon from '@/components/UserIcon';
+
+import { useIsMobile } from '@/hooks/mediaQueries';
+
+import './style.scss';
+
 export const RoadmapPage = () => {
-  return <div className="roadmap-page">Roadmap page</div>;
+  const { user } = useAuthContext() as AuthContextType;
+  const isMobile = useIsMobile();
+
+  const addButtonText = isMobile
+    ? '+ Feedback'
+    : '+ Add Feedback';
+
+  return (
+    <div className="roadmap-page">
+      <header className="roadmap-page__header">
+        <div className="roadmap-page__header-content">
+          <div>
+            <Goback variant="white" />
+            <h1 className="fw-bold roadmap-page__title">Roadmap</h1>
+          </div>
+          <div className="roadmap-page__actions">
+            <AddButton>{addButtonText}</AddButton>
+            {user && <UserIcon />}
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 };
