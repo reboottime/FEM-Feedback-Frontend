c9f7fdd7fff462a73b37758da0ebc457d5fc7116
Kate <3837437+katesroad@users.noreply.github.com>
2023-01-10 21:49:08 -0500

    Add roadmap status description constant

--- src/constants/feedbacks.ts
+++ src/constants/feedbacks.ts
@@ -1,3 +1,21 @@
 export const category = ['UI', 'UX', 'feature', 'bug', 'enhancement'];
 
 export const status = ['new', 'planned', 'in progress', 'live'];
+
+export const ROADMAP_STATUS_DESCRIPTION = {
+  planned: {
+    title: 'Planned',
+    count: 0,
+    description: 'Ideas prioritized for research',
+  },
+  'in progress': {
+    count: 0,
+    description: 'Currently being developed',
+    title: 'In Progress',
+  },
+  live: {
+    count: 0,
+    description: 'Released features',
+    title: 'Live',
+  },
+};
