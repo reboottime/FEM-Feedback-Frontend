8bde92d3a746554ed35e1c51d5e318d6ceab25a8
Kate <3837437+katesroad@users.noreply.github.com>
2023-01-13 02:50:36 -0500

    Add playaround

--- src/pages/feedbacks/ListPage/components/Playaround/Playaround.tsx
+++ src/pages/feedbacks/ListPage/components/Playaround/Playaround.tsx
@@ -1,21 +1,49 @@
 import classNames from 'classnames';
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import { ADMIN_PASS, USER_PASS } from './constants';
+import { useSignInUser, useSignOutUser } from '@/hooks/queries/users';
+
 
 import './style.scss';
 
 export const Playaround: React.FC<Props> = ({ className }) => {
+  const signOutMutation = useSignOutUser();
+  const signInMutation = useSignInUser();
+
+  const handleSignInButtonClick = async (role: 'admin' | 'user') => {
+    await signOutMutation.mutateAsync();
+
+    if (role === 'admin') {
+      signInMutation.mutate(ADMIN_PASS);
+    } else if (role === 'user') {
+      signInMutation.mutate(USER_PASS);
+    }
+  };
+
   return (
     <div className={classNames('playaround border-rounded--large', className)}>
       <div className="playaround__title">
-        <h3 className="typography-heading-3">Play Around</h3>
+        <h3 className="typography-heading-3">Play around</h3>
       </div>
-      <p>
-        <Link className="roadmap__link fw-semi-bold"
-          to="/users">
-          Pick a test account
-        </Link>
-      </p>
+      <ul>
+        <li>
+          <button
+            className="playaround__sign-in typography-body-3 fw-semi-bold"
+            onClick={() => handleSignInButtonClick('user')}
+          >
+            Sign in as user
+          </button>
+        </li>
+        <li>
+          <button
+            className="playaround__sign-in typography-body-3 fw-semi-bold"
+            onClick={() => handleSignInButtonClick('admin')}
+          >
+            Sign in as admin
+          </button>
+        </li>
+      </ul>
     </div>
   );
 };
---
+++ src/pages/feedbacks/ListPage/components/Playaround/constants.ts
@@ -0,0 +1,9 @@
+export const ADMIN_PASS = {
+  username: '',
+  password: '',
+};
+
+export const USER_PASS = {
+  username: '',
+  password: '',
+};
