a52e4587292bd63073e7fefa06bbed7b77f780ad
Kate <3837437+katesroad@users.noreply.github.com>
2023-01-14 00:29:11 -0500

    Fix: resolve race condition that the later coming back metadata has error may redirect user to feedback list page

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
+import { useAuthContext } from './AppProviders';
+
 import { useGetCurrentUser } from '@/hooks/queries/users';
 
 const ProtectedRoute: React.FC<Props> = ({ toPath, children }) => {
+  const { user } = useAuthContext();
   const {
     isSuccess,
     isError
   } = useGetCurrentUser();
 
-  if (isSuccess) {
+  if (user || isSuccess) {
     return children;
   }
 
