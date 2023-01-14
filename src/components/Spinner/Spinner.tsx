6a57a7713ca67ab0b60d7e52a7937f028e6f52fe
Kate <3837437+katesroad@users.noreply.github.com>
2023-01-14 10:31:19 -0500

    Feature: Spinner, add cover modifier to Spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -4,10 +4,11 @@ import { InfinitySpin } from 'react-loader-spinner';
 
 import './style.scss';
 
-export const Spinner: React.FC<Props> = ({ className, center }) => {
+export const Spinner: React.FC<Props> = ({ className, center, cover }) => {
   return (
     <div className={classNames('spinner', className, {
       'spinner--center': center,
+      'spinner--cover': cover,
     })}
     >
       <InfinitySpin color="hsl(230 76% 59% / 100%)"
@@ -19,4 +20,5 @@ export const Spinner: React.FC<Props> = ({ className, center }) => {
 interface Props {
   center?: boolean;
   className?: string;
+  cover?: boolean;
 }
