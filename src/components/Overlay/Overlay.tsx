1424ea271315872de763c6db74c7e78070185b13
Kate <3837437+katesroad@users.noreply.github.com>
2023-01-27 14:36:56 -0500

    feat: :adhesive_bandage: Avoid page content shifting when opening a modal

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -8,30 +8,16 @@ export const Overlay: React.FC<Props> = ({
   className,
   onClick,
   visible = true,
-}) => {
-  useEffect(() => {
-    const prevOverflow = document.body.style.overflow;
-
-    if (visible) {
-      document.body.style.overflow = 'hidden';
-    }
-
-    return () => {
-      document.body.style.overflow = prevOverflow;
-    };
-  }, [visible]);
-
-  return (
-    <Portal>
-      <div
-        className={classNames('overlay', className, {
-          'overlay--visible': visible,
-        })}
-        onClick={onClick}
-      />
-    </Portal>
-  );
-};
+}) => (
+  <Portal>
+    <div
+      className={classNames('overlay', className, {
+        'overlay--visible': visible,
+      })}
+      onClick={onClick}
+    />
+  </Portal>
+);
 
 export interface Props {
   className?: string;
