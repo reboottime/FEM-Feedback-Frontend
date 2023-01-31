3e9cd43dae4154108fa432f820275b5e48eabb8f
Kate <3837437+katesroad@users.noreply.github.com>
2023-01-30 22:03:44 -0500

    feat(i18n): :tada: add language switcher on playaround board

--- src/pages/feedbacks/ListPage/components/Playaround/Playaround.tsx
+++ src/pages/feedbacks/ListPage/components/Playaround/Playaround.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 
 import { ADMIN_PASS, USER_PASS } from './constants';
 import { useSignInUser, useSignOutUser } from '@/hooks/queries/users';
 
 import './style.scss';
+import Button from '@/components/Button';
 
 export const Playaround = () => {
+  const { i18n } = useTranslation();
+
+  const handleSwitchLange = () => {
+    const language = i18n.language;
+
+    if (language === 'de') {
+      i18n.changeLanguage('en');
+    } else {
+      i18n.changeLanguage('de');
+    }
+  };
+
   const signOutMutation = useSignOutUser();
   const signInMutation = useSignInUser();
 
@@ -28,8 +42,10 @@ export const Playaround = () => {
 
   return (
     <div className='playaround border-rounded--large'>
-      <div className="playaround__title">
+      <div className="playaround__title flex-center-around">
         <h3 className="typography-heading-3">Play around</h3>
+        <button onClick={handleSwitchLange}
+        >{i18n.language}</button>
       </div>
       <ul>
         <li>
